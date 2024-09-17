41e4a9452052552709be71a057b6297638bf40b2
Domingos Julio Pinto <domingosjuliopinto@gmail.com>
2024-09-17 23:54:22 +0530

    Store Version 0.5.0

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -19,7 +19,28 @@ function About() {
                     borderRadius: borderRadiusLG,
                 }}
             >
-            About
+            <h2>About</h2>
+            <hr/>
+            <p>
+                DJPF is just an hobby. This helped me to develop a website, thus making me to learn and practice ReactJS and combine it with 
+                the characters and storylines that I drew/planned a long time ago. A small request, please do not use the characters and 
+                storylines as your own creation. However you can use the characters in your story as long as you give the credits. The characters 
+                and their names, story and storylines, locations are all fictional and do not represent anyone, anything or any place in real life. 
+                Any resemblance is completely coincidence. You can check out DJPF books on <a href='https://www.wattpad.com/user/DJP-Fiction'>Wattpad</a>.
+                <br/>
+                Below are the sites that I used to help me create contents for this website
+                <ul>
+                    <li><a href='https://www.iloveimg.com/resize-image/resize-png'>https://www.iloveimg.com/resize-image/resize-png</a></li>
+                    <li><a href='https://squaremyimage.com/'>https://squaremyimage.com/</a></li>
+                    <li><a href='https://favicon.io/favicon-converter/'>https://favicon.io/favicon-converter/</a></li>
+                    <li><a href='https://www.fotor.com/images/create'>https://www.fotor.com/images/create</a></li>
+                    <li><a href='https://imagecompressor.com/'>https://imagecompressor.com/</a></li>
+                    <li><a href='https://www.iloveimg.com/compress-image'>https://www.iloveimg.com/compress-image</a></li>
+                    <li><a href='https://vectorizer.ai/'>https://vectorizer.ai/</a></li>
+                    <li><a href='https://svgconverter.app/free'>https://svgconverter.app/free</a></li>
+                    <li><a href='https://imagy.app/change-image-to-16x9-aspect-ratio/'>https://imagy.app/change-image-to-16x9-aspect-ratio/</a></li>
+                </ul>
+            </p>
             </Content>
         </div>
     )
--- src/utils/image_links/concept_art/aayiBarboozaNLhackConcept.js
+++ src/utils/image_links/concept_art/aayiBarboozaNLhackConcept.js
@@ -7,7 +7,7 @@ import aayiBarboozaNLhackConcept_mini from '../../while_loading/concept_art/aayi
 // Packaging
 const aayi_Barbooza_NLhack_Concept = []
 
-aayi_Barbooza_NLhack_Concept.push([aayiBarboozaNLhackConcept,aayiBarboozaNLhackConcept_mini])
+aayi_Barbooza_NLhack_Concept.push([aayiBarboozaNLhackConcept,aayiBarboozaNLhackConcept_mini,"NLhack"])
 
 // Exporting
 export {aayi_Barbooza_NLhack_Concept}
